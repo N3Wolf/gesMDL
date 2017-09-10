d4191064b21020ae135bf47b6a6a811195310723
Ederson Sparenberger <edersons@hotmail.com>
2017-09-10 17:50:38 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Conecta o mongoose.
+mongoose.Promisse = global.Promisse;
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database '+config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error: '+err);
+});
+
+// Tipo do App
+const app = express();
+
+// Declaração da rota do users
+const users = require('./routes/users');
+
+// Port number
+const port = process.env.PORT || 8080;
+
+// CORS middleware
+app.use(cors());
+
+// Set static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// bodyParser middleware
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+// Index route
+app.get('/', (req,res) => {
+  res.send('Invalid Endpoint');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log('Server started on port '+port);
+});
