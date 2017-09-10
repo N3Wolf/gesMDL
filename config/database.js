c9f29d041a4f96bfa6ed3ecd7a2b459b21163396
Ederson Sparenberger <edersons@hotmail.com>
2017-09-10 18:16:34 -0300

    adicionado conexão mongodb na mLab

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/gesMDL',
+ database: 'mongodb://gesmdl:ges123456@ds133104.mlab.com:33104/gesmdl'
+
+  // database: 'mongodb://localhost:27017/gesMDL',
   secret: 'stringsegredo'
 }
